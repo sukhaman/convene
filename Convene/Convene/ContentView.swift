726466ba27e704018f60621c76e8c2878732e3f7
Sukhaman Singh <sukhamandev@gmail.com>
2024-07-04 10:05:06 -0400

    project init

---
+++ Convene/Convene/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Convene
+//
+//  Created by Sukhaman Singh on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Convene/Convene/ConveneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConveneApp.swift
+//  Convene
+//
+//  Created by Sukhaman Singh on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ConveneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
